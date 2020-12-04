6727a3a3354aef4f623873572a649597c058c916
Kiki <Kiki@DESKTOP-SLVGKDP>
2020-12-04 22:01:39 +0700

    Upload Project

---
+++ src/uts/if2/pkg10119052/rizkidwinugraha/no3/DeathCoreKepiting.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119052.rizkidwinugraha.no3;
+
+/**
+ *
+ * @author Kiki
+ */
+public class DeathCoreKepiting {
+    
+}
---
+++ src/uts/if2/pkg10119052/rizkidwinugraha/no3/UTSIF210119052RIZKIDWINUGRAHANo3.java
@@ -0,0 +1,17 @@
+package uts.if2.pkg10119052.rizkidwinugraha.no3;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program Genre Musik
+ * 
+ */
+public class UTSIF210119052RIZKIDWINUGRAHANo3 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
